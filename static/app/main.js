dc6fc31d1e8f6c01ddb8264c45ab54b70fcc4b7e
落雨不悔 <m17762618644@163.com>
2022-11-06 23:26:53 +0800

    文件夹获取

--- static/app/main.js
+++ static/app/main.js
@@ -28,11 +28,6 @@ kodReady.push(function(){
     });
 
 
-    // 在右键新建中增加新建类型
-    Events.bind('rightMenu.newFileAdd',function(menuList){
-        menuList.push({type:'md',name:'{{package.name}}',createOpen:1,appName:'{{package.id}}'});
-    });
-
     // 增加路由
     Router.mapIframe({page:'{{package.id}}',title:'{{package.name}}',url:'{{pluginApi}}',ignoreLogin:1});
 
@@ -49,17 +44,4 @@ kodReady.push(function(){
         }
     });
 
-    Events.bind('aceEditor.fileOpenModeInit',function(modeList){
-        let xml = 'md'
-        modeList.xml = modeList.xml ? modeList.xml + ','+xml:xml;
-        modeList.json = modeList.json ? modeList.json + ',pos':'pos';
-    });
-
-    // 增加样式
-    $.addStyle("\
-	.x-item-icon.x-pos{background-image:url('{{staticPath}}image/file_icon/icon_app/on.png');}\
-	.x-item-icon.x-xmind{background-image:url('{{pluginHost}}static/image/xmind.png');}\
-	.x-item-icon.x-graphml{background-image:url('{{pluginHost}}static/image/graphml.png');}\
-	.x-item-icon.x-drawio{background-image:url('{{pluginHost}}static/image/icon.png');}\
-	");
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,92 @@
+function getCookie(name) {
+    return document.cookie.match(`[;\s+]?${name}=([^;]*)`)?.pop();
+}
+
+// 比如cookie如下： a=b; c=d
+// 使用
+getCookie('c') // d
+
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'rain 编辑器'
+        folder: '',
+        title: 'rain 编辑器',
+        instance: null,
+        theme: 'light',
+        config: {
+            path: '',
+            name: '',
+            display: ''
+        },
+        folderList: [],
+        fileList: []
+    },
+    created() {
+        // 初始化时判断当前主题
+        let theme = window.parent.document.getElementsByTagName('html')[0].getAttribute('data-theme')
+        document.getElementById('app').setAttribute('theme', theme)
+        this.theme = theme === 'light' ? 'classic' : 'dark';
+        // 获取文件夹信息
+        let result = localStorage.getItem('rain-note');
+        try {
+            let config = JSON.parse(result);
+            this.config.path = config.path;
+            this.config.name = config.name;
+            this.config.display = config.pathDisplay;
+        } catch (ignore) { }
+        if (this.config.path != '') {
+            // 获取文件
+            this.ls(this.config.path)
+        }
+    },
+    mounted() {
+        // 挂载时初始化编辑器
+        this.instance = new Vditor('editor', {
+            theme: this.theme,
+            width: '100%',
+            height: 'calc(100vh - 31px - 31px)',
+            value: '请开始你的表演',
+            cache: {
+                enable: false
+            }
+        })
+    },
+    methods: {
+        openFolder() {
+            let that = this;
+            kodApi.fileSelect({
+                title: "选择文件夹",
+                type: 'folder',
+                callback: function (result) {
+                    localStorage.setItem('rain-note', JSON.stringify(result));
+                    // 赋值
+                    this.config.path = config.path;
+                    this.config.name = config.name;
+                    this.config.display = config.pathDisplay;
+                    // 展示文件夹
+                    that.ls(this.config.path)
+                }
+            });
+        },
+        ls(path) {
+            let that = this;
+            $.post('/index.php?explorer/list/path', {
+                path: path,
+                page: 1,
+                pageNum: 500,
+                CSRF_TOKEN: getCookie('CSRF_TOKEN'),
+                API_ROUTE: 'explorer/list/path'
+            }, function (response) {
+                let folderList = [];
+                for(let folder of response.data.folderList) {
+                    // 文件夹渲染
+                    folderList.push({
+                        name: folder.name,
+                        path: folder.path
+                    })
+                }
+                that.folderList=  folderList;
+            })
+        }
     }
 });
-
-// 初始化时判断当前主题
-let theme = window.parent.document.getElementsByTagName('html')[0]
-.getAttribute('data-theme')
-document.getElementById('app').setAttribute('theme', theme)
